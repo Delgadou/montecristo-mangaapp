2292b1cad9e0b5a0e7b30712ecf0931f0e288cec
Henrique Delgado <delgado.ohenrique@gmail.com>
2024-12-16 08:37:02 -0300

    first commit

---
+++ montecristo-manga/montecristo_mangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  montecristo_mangaApp.swift
+//  montecristo-manga
+//
+//  Created by Henrique Delgado on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct montecristo_mangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
